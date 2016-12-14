7cec53f2e2f94c982dff4d220f23cd5a261b5cd4
Stelios Koussouris <skousou@gmail.com>
2016-12-14 14:03:04 +0000

    ORDERING Check

--- alabaster-snowball/src/main/java/emea/summit/architects/RequestPayloadComparator.java
+++ alabaster-snowball/src/main/java/emea/summit/architects/RequestPayloadComparator.java
@@ -6,6 +6,8 @@ public class RequestPayloadComparator implements Comparator<RequestPayload> {
 
 	@Override
 	public int compare(RequestPayload p1, RequestPayload p2) {
+		String where = p1.getReindeerName().compareTo(p2.getReindeerName()) < 0 ? "BEFORE" : "AFTER";
+		System.out.println("["+p1.getReindeerName()+"] <"+where+"> ["+p2.getReindeerName()+"]");
 		return p1.getReindeerName().compareTo(p2.getReindeerName());
 	}
 
